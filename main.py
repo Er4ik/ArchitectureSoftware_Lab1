42a4273b9b4f107575956f3a0630a48cc890888d
Per4ik <ervin.osmanov.007@gmail.com>
2022-02-19 12:09:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from interactive import inputCoefficient
 from common.helpers.quadraticEquation import calculateFunction, is_adv_digit
 
 def main():
-    programType = input('Select program type Intercactive - i or Noninteractive - ni: ')
+    programType = input('Select program type: Interactive - i or Noninteractive - ni: ')
     if(programType == 'i'):
         a, b, c = inputCoefficient()
         calculateFunction(a, b, c)
@@ -15,7 +15,7 @@ def main():
             print(f'Equation is: ({validCoeffs[0]}) x^2 + ({validCoeffs[1]}) x + {validCoeffs[2]} = 0')
             calculateFunction(*validCoeffs)
     else:
-        print('Wrong progpar type!')
+        print('Wrong program type!')
         main()
 
 main()
